30cb959c80580ae6ff16f8a2cb30f5186f5aa4d1
aibou <aibou7251@gmail.com>
2015-02-20 19:55:22 +0900

    Implement provisionally

--- lib/scarecrow.rb
+++ lib/scarecrow.rb
@@ -1,5 +1,20 @@
-require "scarecrow/version"
+require 'sinatra/base'
+
+require 'scarecrow/version'
 
 module Scarecrow
-  # Your code goes here...
+  def self.run
+    # eat yaml file
+    app = Sinatra.new do
+      get '/' do
+        'hello, world'
+      end
+    end
+
+    # setup definition
+    # run sinatra
+    app.run! port: 7874
+  end
 end
+
+Scarecrow.run
