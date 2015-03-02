12a33287cdc924b7d24efa34579b01ef10dc1874
aibou <aibou7251@gmail.com>
2015-03-02 19:00:41 +0900

    Implement for new specification

--- lib/scarecrow/formatter.rb
+++ lib/scarecrow/formatter.rb
@@ -1,21 +1,21 @@
 module Scarecrow
   module Formatter
-    def self.format! data
+    def self.format data
       formatted = {}
       
       data.each do |key, value|
         path = File.join('/', key.gsub(/\./, '/'))
-        formatted[path] = value.symbolize_keys!
-      end
-      
-      formatted.each_key do |path|
-        formatted[path][:method] = formatted[path][:method].downcase.to_sym
-        formatted[path][:patterns].map! do |pattern|
-          pattern[:request] ||= {}
-          pattern[:request][:params] ||= {}
-          pattern[:request][:headers] ||= {}
-          pattern[:response] ||= {}
-          pattern
+        formatted[path] = {}
+        value.each do |method, patterns|
+          patterns.map! do |pattern|
+            pattern.symbolize_keys!
+            pattern[:request] ||= {}
+            pattern[:request][:params] ||= {}
+            pattern[:request][:headers] ||= {}
+            pattern[:response] ||= {}
+            pattern
+          end
+          formatted[path][method.downcase.to_sym] = patterns
         end
       end
       formatted
--- lib/scarecrow/server.rb
+++ lib/scarecrow/server.rb
@@ -4,32 +4,34 @@ module Scarecrow
   module Server
     def self.define hash
       app = Sinatra.new
-      formatted = Scarecrow::Formatter.format! hash
+      formatted = Scarecrow::Formatter.format hash
 
       formatted.each do |path, data|
-        app.send(data[:method], path)do
-          data[:patterns].each do |pattern|
-            conds = []
-            pattern[:request][:params].each do |name, value|
-              conds << params[name].eql?(value)
+        data.each do |method, patterns|
+          app.send(method, path) do
+            patterns.each do |pattern|
+              conds = []
+              pattern[:request][:params].each do |name, value|
+                conds << params[name].eql?(value)
+              end
+              
+              pattern[:request][:headers].each do |name, value|
+                conds << request.env["HTTP_#{name.upcase}"].eql?(value)
+              end
+              
+              if conds.all?
+                status pattern[:response][:status] || 200
+                headers pattern[:response][:header] || {}
+                body pattern[:response][:body] || ''
+                return
+              end
             end
             
-            pattern[:request][:headers].each do |name, value|
-              conds << request.env["HTTP_#{name.upcase}"].eql?(value)
-            end
-
-            if conds.all?
-              status pattern[:response][:status] || 200
-              headers pattern[:response][:header] || {}
-              body pattern[:response][:body] || ''
-              return
-            end
+            # default
+            status 404
+            headers({ 'X-Scarecrow-Status' => 'No such pattern' })
+            body 'No such pattern'
           end
-          
-          # default
-          status 404
-          headers({ 'X-Scarecrow-Status' => 'No such pattern' })
-          body 'No such pattern'
         end
       end
       app
